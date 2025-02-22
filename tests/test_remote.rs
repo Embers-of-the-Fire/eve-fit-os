e93136e1f88670c9f350f8089f88e18bc3d66096
Embers-of-the-Fire <stellarishs@163.com>
2025-02-22 14:25:02 +0800

    add: add remote repair attr

---
+++ tests/test_remote.rs
@@ -0,0 +1,104 @@
+use std::collections::HashMap;
+use std::fs::File;
+
+use eve_fit_os::calculate::calculate;
+use eve_fit_os::constant::patches::attr::{
+    ATTR_ARMOR_REMOTE_REPAIR_RATE, ATTR_HULL_REMOTE_REPAIR_RATE,
+    ATTR_REMOTE_CAPACITOR_TRANSMITTER_RATE, ATTR_SHIELD_REMOTE_BOOST_RATE,
+};
+use eve_fit_os::fit::{
+    FitContainer, ItemFit, ItemModule, ItemSlot, ItemSlotType, ItemState,
+};
+use eve_fit_os::protobuf::Database;
+
+#[test]
+fn test_remote() {
+    let skill_all_5: HashMap<i32, u8> = {
+        let rdr =
+            File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/skills.json")).unwrap();
+        serde_json::from_reader(rdr).unwrap()
+    };
+
+    let fit = ItemFit {
+        ship_type_id: 628,
+        modules: vec![
+            ItemModule {
+                type_id: 4299,
+                slot: ItemSlot {
+                    slot_type: ItemSlotType::Medium,
+                    index: 0,
+                },
+                state: ItemState::Active,
+                charge: None,
+            },
+            ItemModule {
+                type_id: 3588,
+                slot: ItemSlot {
+                    slot_type: ItemSlotType::Medium,
+                    index: 0,
+                },
+                state: ItemState::Active,
+                charge: None,
+            },
+            ItemModule {
+                type_id: 26912,
+                slot: ItemSlot {
+                    slot_type: ItemSlotType::Medium,
+                    index: 0,
+                },
+                state: ItemState::Active,
+                charge: None,
+            },
+            ItemModule {
+                type_id: 1190,
+                slot: ItemSlot {
+                    slot_type: ItemSlotType::Medium,
+                    index: 0,
+                },
+                state: ItemState::Active,
+                charge: None,
+            },
+        ],
+        drones: vec![],
+        implants: vec![],
+    };
+
+    let container = FitContainer::new(fit, skill_all_5);
+
+    let info =
+        Database::init(concat!(env!("CARGO_MANIFEST_DIR"), "/data/out/pb2")).unwrap();
+
+    let out = calculate(&container, &info);
+
+    let rep = out
+        .hull
+        .attributes
+        .get(&ATTR_HULL_REMOTE_REPAIR_RATE)
+        .and_then(|t| t.value)
+        .unwrap_or_default();
+    println!("remote hull repair damage: {:?}", rep);
+
+    let rep = out
+        .hull
+        .attributes
+        .get(&ATTR_SHIELD_REMOTE_BOOST_RATE)
+        .and_then(|t| t.value)
+        .unwrap_or_default();
+    println!("remote shield repair damage: {:?}", rep);
+
+    let rep = out
+        .hull
+        .attributes
+        .get(&ATTR_ARMOR_REMOTE_REPAIR_RATE)
+        .and_then(|t| t.value)
+        .unwrap_or_default();
+    println!("remote armor repair damage: {:?}", rep);
+
+    let rep = out
+        .hull
+        .attributes
+        .get(&ATTR_REMOTE_CAPACITOR_TRANSMITTER_RATE)
+        .and_then(|t| t.value)
+        .unwrap_or_default();
+    println!("remote capacitor transmit: {:?}", rep);
+}
