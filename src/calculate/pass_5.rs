bfd800a56275b4242867a7e523eb3cce1123258d
Embers-of-the-Fire <stellarishs@163.com>
2025-05-18 16:27:48 +0800

    fix: fix chargeable armor repairer repair amount

--- src/calculate/pass_5.rs
+++ src/calculate/pass_5.rs
@@ -3,6 +3,7 @@ use super::{Item, Ship};
 use crate::provider::{FitProvider, InfoProvider};
 
 mod capacitor;
+mod chargeable_armor_repairer;
 
 impl Item {
     pub fn add_attribute(&mut self, attribute_id: i32, base_value: f64, value: f64) {
@@ -13,10 +14,7 @@ impl Item {
 }
 
 /// Attributes don't contain all information displayed, so we calculate some fake attributes with those values.
-pub(super) fn pass(
-    _fit: &impl FitProvider,
-    _info: &impl InfoProvider,
-    ship: &mut Ship,
-) {
+pub(super) fn pass(fit: &impl FitProvider, _info: &impl InfoProvider, ship: &mut Ship) {
     capacitor::attribute_capacitor_depletes_in(ship);
+    chargeable_armor_repairer::attribute_chargeable_armor_repairer(fit, ship);
 }
---
+++ src/calculate/pass_5/chargeable_armor_repairer.rs
@@ -0,0 +1,26 @@
+use crate::{calculate::Ship, provider::FitProvider};
+
+const TYPE_ID_ARMOR_REPAIRER_CHARGE: i32 = 28668;
+const ATTR_ARMOR_MULT: i32 = 1886;
+const ATTR_ARMOR_AMO: i32 = 84;
+
+pub fn attribute_chargeable_armor_repairer(fit: &impl FitProvider, ship: &mut Ship) {
+    for module in ship.modules.iter_mut() {
+        if module
+            .charge
+            .as_ref()
+            .is_some_and(|c| c.item_id.as_type_id(fit) == TYPE_ID_ARMOR_REPAIRER_CHARGE)
+        {
+            let Some(mult) = module.attributes.get(&ATTR_ARMOR_MULT) else {
+                continue;
+            };
+            let Some(amo) = module.attributes.get(&ATTR_ARMOR_AMO) else {
+                continue;
+            };
+            module.set_attribute(
+                ATTR_ARMOR_AMO,
+                amo.value.unwrap_or(0.0) * mult.value.unwrap_or(0.0),
+            );
+        }
+    }
+}
